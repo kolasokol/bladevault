68779cabdda222a4891ae6c6c3ed8520b7ef5470
Kola Sokol <maiden.tech@gmail.com>
2026-07-14 16:29:23 +0300

    Remove deprecated settings prototype components and shared utilities

--- app/settings-prototype/copilot/page.tsx
+++
@@ -1,9 +0,0 @@
-import CopilotPrototype from '@/components/settings-prototypes/copilot'
-
-export default function CopilotPrototypePage() {
-  return (
-    <div className="h-full">
-      <CopilotPrototype />
-    </div>
-  )
-}
--- app/settings-prototype/lm-studio/page.tsx
+++
@@ -1,9 +0,0 @@
-import LmStudioPrototype from '@/components/settings-prototypes/lm-studio'
-
-export default function LmStudioPrototypePage() {
-  return (
-    <div className="flex h-full items-start justify-center p-4">
-      <LmStudioPrototype />
-    </div>
-  )
-}
